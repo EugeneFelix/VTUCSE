5e0a691dc1e15afa1eb89858e7bcc25efd977a64
EugeneFelix <ontharros@gmail.com>
2022-03-01 23:18:46 +0530

    commit

---
+++ YearII/Semester03/DSA/DSL08.c
@@ -0,0 +1,310 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int MAX = 4, count;
+
+struct node
+{
+	int ssn;
+	char name[20];
+	char dept[10];
+	char design[10];
+	int sal;
+	char phno[10];
+	struct node *llink;
+	struct node *rlink;
+};
+typedef struct node* NODE;
+
+int countnodes(NODE first)
+{
+	NODE temp;
+	count = 0;
+	temp = first;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->rlink;
+	}
+	return count;
+}
+NODE getnode()
+{
+	NODE temp;
+	temp = malloc(sizeof(struct node));
+	temp->rlink = NULL;
+	temp->llink = NULL;
+	printf("enter SSN\n");
+	scanf("%d", &temp->ssn);
+	printf("enter name\n");
+	scanf("%s", temp->name);
+	printf("enter dept\n");
+	scanf("%s", temp->dept);
+	printf("enter designation\n");
+	scanf("%s", temp->design);
+	printf("enter salary\n");
+	scanf("%d", &temp->sal);
+	printf("enter phno\n");
+	scanf("%s", temp->phno);
+	return temp;
+}
+void display(NODE first)
+{
+	NODE temp;
+	if (first == NULL)
+	{
+		printf("No employee data\n");
+	}
+	else
+	{
+		temp = first;
+		printf("employee data\n");
+		printf("SSN\t Name\t Department\t Designation\t Salary\t Phone no\n");
+		while (temp != NULL)
+		{
+			printf("\n%d\t%s\t%s\t%s\t%d\t%s", temp->ssn, temp->name, temp->dept, temp->design, temp->sal, temp->phno);
+			temp = temp->rlink;
+		}
+		printf("\n the no.of nodes is %d\n", countnodes(first));
+	}
+}
+
+NODE create(NODE first)
+{
+	NODE temp, cur;
+	if (first == NULL)
+	{
+		temp = getnode();
+		first = temp;
+		return first;
+	}
+	else
+	{
+		temp = getnode();
+		cur = first;
+		while (cur->rlink != NULL)
+		{
+			cur = cur->rlink;
+		}
+		cur->rlink = temp;
+		temp->llink = cur;
+	}
+	return first;
+}
+
+NODE insert_front(NODE first)
+{
+	NODE temp;
+	if (countnodes(first) == MAX)
+	{
+		printf("List is full/overflow\n");
+	}
+	else
+	{
+		if (first == NULL)
+		{
+			temp = getnode();
+			first = temp;
+		}
+		else
+		{
+			temp = getnode();
+			temp->rlink = first;
+			first->llink = temp;
+			first = temp;
+		}
+	}
+	return first;
+}
+NODE insert_rear(NODE first)
+{
+	if (countnodes(first) == MAX)
+	{
+		printf("list is full/overflow\n");
+	}
+	else
+	{
+		first = create(first);
+	}
+	return first;
+}
+
+NODE insertnode(NODE first)
+{
+	int ch;
+	while (1)
+	{
+		printf("\n1.insert front\n 2.insert rear\n 3.exit\n");
+		printf("enter your choice\n");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+		case 1:
+			first = insert_front(first);
+			display(first);
+			break;
+		case 2:
+			first = insert_rear(first);
+			display(first);
+			break;
+		case 3:
+			return first;
+		}
+	}
+	return first;
+}
+
+NODE delete_front(NODE first)
+{
+	NODE temp;
+	if (first == NULL)
+	{
+		printf("list is empty/underflow\n");
+	}
+	else if (first->rlink == NULL)
+	{
+		free(first);
+		printf("first node deleted\n");
+		first = NULL;
+		return first;
+	}
+	else
+	{
+		temp = first;
+		first = first->rlink;
+		first->llink = NULL;
+		free(temp);
+		printf("first node is deleted\n");
+	}
+	return first;
+}
+
+NODE delete_rear(NODE first)
+{
+	NODE cur, prev;
+	if (first == NULL)
+	{
+		printf("list is empty/underflow\n");
+	}
+	else if (first->rlink == NULL)
+	{
+		free(first);
+		printf("last node deleted\n");
+		first = NULL;
+		return first;
+	}
+	else
+	{
+		cur = first;
+		prev = NULL;
+		while (cur->rlink != NULL)
+		{
+			prev = cur;
+			cur = cur->rlink;
+		}
+		prev->rlink = NULL;
+		free(cur);
+		printf("last node deleted\n");
+	}
+	return first;
+}
+
+NODE deletenode(NODE first)
+{
+	int ch;
+	while (1)
+	{
+		printf("1.delete front\n 2.delete rear\n 3.exit\n");
+		printf("enter your choice\n");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+		case 1:
+			first = delete_front(first);
+			display(first);
+			break;
+		case 2:
+			first = delete_rear(first);
+			display(first);
+			break;
+		case 3:
+			return first;
+		}
+	}
+	return first;
+}
+
+NODE dequeue(NODE first)
+{
+	int ch;
+	while (1)
+	{
+		printf("1.insert rear\n 2.insert front\n 3.delete rear\n 4.delete front\n 5:exit\n");
+		printf("enter your choice\n");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+		case 1:
+			first = insert_rear(first);
+			display(first);
+			break;
+		case 2:
+			first = insert_front(first);
+			display(first);
+			break;
+		case 3:
+			first = delete_rear(first);
+			display(first);
+			break;
+		case 4:
+			first = delete_front(first);
+			display(first);
+			break;
+		case 5:
+			return first;
+		}
+	}
+	return first;
+}
+
+int main()
+{
+	int ch, i, n;
+	NODE first = NULL;
+	printf("\n-_-_-_-_-_-_-_-_-_-_Employee Database-_-_-_-_-_-_-_-_-_\n");
+	while (1)
+	{
+		printf("\n\t 1.Create\n\t 2.Display\n\t 3.Insert\n\t 4.Delete\n\t 5.Dequeue\n\t 6.Exit\n");
+		printf("\t\t  Enter your choice\n");
+		scanf("%d", &ch);
+		switch (ch)
+		{
+		case 1:
+			printf("How many employees do you want to create\n");
+			scanf("%d", &n);
+			for (i = 0; i < n; i++)
+			{
+				first = create(first);
+			}
+			break;
+		case 2:
+			display(first);
+			break;
+		case 3:
+			first = insertnode(first);
+			break;
+		case 4:
+			first = deletenode(first);
+			break;
+		case 5:
+			first = dequeue(first);
+			break;
+		case 6:
+			exit(0);
+		default:
+			printf("Invalid choice! Please try agai!\n");
+		}
+	}
+	return 0;
+}
---
+++ YearII/Semester03/DSA/DSL09.c
@@ -0,0 +1,186 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+struct node
+{
+	int coeff, expx, expy, expz, flag;
+	struct node *link;
+};
+typedef struct node *NODE;
+NODE getnode();
+NODE read_poly(NODE head);
+NODE insert_rear(int coeff, int expx, int expy, int expz, NODE head);
+void display(NODE head);
+NODE add_poly(NODE head1, NODE head2, NODE head3);
+void evaluate(NODE head);
+NODE getnode()
+{
+	NODE x;
+	x = (NODE)malloc(sizeof(struct node));
+	if (x == NULL)
+	{
+		printf("insufficient memory\n");
+		exit(0);
+	}
+	return x;
+}
+NODE read_poly(NODE head)
+{
+	int expx, expy, expz, coeff, ch = 1;
+	while (ch != 0)
+	{
+		printf("enter the coeff\n");
+		scanf("%d", &coeff);
+		printf("enter power ofx\n");
+		scanf("%d", &expx);
+		printf("enter the power y\n");
+		scanf("%d", &expy);
+		printf("enter the power z\n");
+		scanf("%d", &expz);
+		head = insert_rear(coeff, expx, expy, expz, head);
+		printf("press 1 to enter one more item or 0 to end\n");
+		scanf("%d", &ch);
+	}
+	return head;
+}
+NODE insert_rear(int coeff, int expx, int expy, int expz, NODE head)
+{
+	NODE temp, cur;
+	temp = getnode();
+	temp->coeff = coeff;
+	temp->expx = expx;
+	temp->expy = expy;
+	temp->expz = expz;
+	temp->link = NULL;
+	cur = head->link;
+	while (cur->link != head)
+	{
+		cur = cur->link;
+	}
+	cur->link = temp;
+	temp->link = head;
+	return head;
+}
+void display(NODE head)
+{
+	NODE temp;
+	if (head->link == head)
+	{
+		printf("polynomial doesn't exixt\n");
+	}
+	else
+	{
+		temp = head->link;
+		while (temp != head)
+		{
+			printf("%dx^%dy^%dz^%d", temp->coeff, temp->expx, temp->expy, temp->expz);
+			if (temp->link != head)
+			{
+				printf("+");
+			}
+			temp = temp->link;
+		}
+		printf("\n");
+	}
+}
+NODE add_poly(NODE head1, NODE head2, NODE head3)
+{
+	NODE temp1, temp2;
+	int x1, x2, y1, y2, z1, z2, coeff1, coeff2, coeff;
+	temp1 = head1->link;
+	while (temp1 != head1)
+	{
+		x1 = temp1->expx;
+		y1 = temp1->expy;
+		z1 = temp1->expz;
+		coeff1 = temp1->coeff;
+		temp2 = head2->link;
+		while (temp2 != head2)
+		{
+			x2 = temp2->expx;
+			y2 = temp2->expy;
+			z2 = temp2->expz;
+			coeff2 = temp2->coeff;
+			if (x1 == x2 && y1 == y2 && z1 == z2)
+				break;
+			temp2 = temp2->link;
+		}
+		if (temp2 != head2)
+		{
+			coeff = coeff1 + coeff2;
+			temp2->flag = 1;
+			if (coeff != 0)
+				head3 = insert_rear(coeff, x1, y1, z1, head3);
+		}
+		else
+			head3 = insert_rear(coeff1, x1, y1, z1, head3);
+		temp1 = temp1->link;
+	}
+	temp2 = head2->link;
+	while (temp2 != head2)
+	{
+		if (temp2->flag == 0)
+		{
+			head3 = insert_rear(temp2->coeff, temp2->expx, temp2->expy, temp2->expz, head3);
+		}
+		temp2 = temp2->link;
+	}
+	return head3;
+}
+void evaluate(NODE head)
+{
+	NODE temp;
+	int x, y, z;
+	double result = 0;
+	printf("enter x,y,z values\n");
+	scanf("%d%d%d", &x, &y, &z);
+	temp = head->link;
+	while (temp != head)
+	{
+		result = result + (temp->coeff * pow(x, temp->expx) * pow(y, temp->expy) * pow(z, temp->expz));
+		temp = temp->link;
+	} printf("Polynomial evaluated result is %f\n",result); }
+int main() {
+		NODE head, head1, head2, head3;
+		int ch;
+		head = getnode();
+		head1 = getnode();
+		head2 = getnode();
+		head3 = getnode();
+		head->link = head;
+		head1->link = head1;
+		head2->link = head2;
+		head3->link = head3;
+		while (1)
+		{
+printf("\tMENU\n1.evaluate polynomial\n2.polynomail addition\n3.exit\n");
+printf("enter your choice\n");
+scanf("%d",&ch);
+switch(ch) {
+			case 1:
+				printf("enter polynomial to evaluate\n");
+				head = read_poly(head);
+				display(head);
+				evaluate(head);
+				break;
+			case 2:
+				printf("enter the first polynomial\n");
+				head1 = read_poly(head1);
+				printf("enter the second polynomial\n");
+				head2 = read_poly(head2);
+				head3 = add_poly(head1, head2, head3);
+				printf("first polynomial is\n");
+				display(head1);
+				printf("second polynomial is\n");
+				display(head2);
+				printf("the sum of 2 polynomials is\n");
+				display(head3);
+				break;
+			case 3:
+				exit(0);
+			default:
+				printf("invalid choice\n");
+		}
+		}
+		return 0;
+}
