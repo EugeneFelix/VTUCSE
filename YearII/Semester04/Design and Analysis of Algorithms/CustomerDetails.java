60e08fbb6189bfe46c6c058ad6545831c5bba9c6
EugeneFelix <ontharros@gmail.com>
2022-05-14 21:49:41 +0530

    commit

--- YearII/Semester04/Design and Analysis of Algorithms/CustomerDetails.java
+++ YearII/Semester04/Design and Analysis of Algorithms/CustomerDetails.java
@@ -1,11 +1,16 @@
 package com.vtucse_iv;
 
-import java.util.Scanner; import java.util.StringTokenizer;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 class Customer {
-private String customerName,date; public Customer(String customerName, String date) {
+private String customerName,date;
+public Customer(String customerName, String date) {
 super();
 this.customerName = customerName; this.date = date;
-} @Override
+}
+@Override
+
 public String toString() {
 String returnValue = customerName+","; StringTokenizer tokenizer = new StringTokenizer(date,"/"); System.out.println("The Customer details are "); while(tokenizer.hasMoreTokens()) {
 returnValue = returnValue+tokenizer.nextToken(); if(tokenizer.hasMoreElements()) {
