2b4720bdde9af85022907ed169a80772bb0881a6
EugeneFelix <ontharros@gmail.com>
2022-09-09 18:55:27 +0530

    commit

--- YearII/Semester04/Design and Analysis of Algorithms/Version 2/Subset.java
+++ YearII/Semester04/Design and Analysis of Algorithms/Version 2/Subset.java
@@ -1,46 +1,46 @@
 import java.util.Scanner;
 public class Subset {
 	static int count = 0, d, n;
-	static int w[] = new int[10];
-	static int x[] = new int[10];
+	static int set[] = new int[10];
+	static int isSafe[] = new int[10];
 	public void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int sum = 0, i;
+		int target = 0, i;
 		System.out.println("Enter the value of n");
 		n = in.nextInt();
 		System.out.println("Enter the elements");
 		for (i = 1; i <= n; i++)
-			w[i] = in.nextInt();
+			set[i] = in.nextInt();
 		System.out.println("Enter the value of d");
 		d = in.nextInt();
 		for (i = 1; i <= n; i++)
-			x[i] = 0;
+			isSafe[i] = 0;
 		for (i = 1; i <= n; i++)
-			sum += w[i];
-		if (d > sum) {
+			target += set[i];
+		if (d > target) {
 			System.out.println("No Solution");
 		} else {
-			subset(0, 1, sum);
+			subset(0, 1, target);
 			if (count == 0)
 				System.out.println("No solution");
 		}
 	}
-	public void subset(int s, int k, int sum) {
-		x[k] = 1;
-		if (s + w[k] == d) {
-			count++;
-			System.out.println("Subset" + count);
-			for (int i = 1; i <= k; i++)
-				if (x[i] == 1)
-					System.out.println(w[i]);
-		} else
-				if (s + w[k] + w[k + 1] <= d)
-			subset(s + w[k], k + 1, sum - w[k]);
-
-
-		if ( d <= (s + sum - w[k])&& d >= (s + w[k + 1])) {
-			x[k] = 0;
-			subset(s, k + 1, sum - w[k]);
+	public void subset(int s, int k, int target) {
+		isSafe[k] = 1;
+		if (s + set[k] == d)
+			printSolution(k);
+		else if (s + set[k] + set[k + 1] <= d)
+			subset(s + set[k], k + 1, target - set[k]);
+		if (d <= (s + target - set[k]) && d >= (s + set[k + 1])) {
+			isSafe[k] = 0;
+			subset(s, k + 1, target - set[k]);
 		}
+	}
+	public void printSolution(int k) {
+		count++;
+		System.out.println("Subset " + count + ":");
+		for (int i = 1; i <= k; i++)
+			if (isSafe[i] == 1)
+				System.out.println(set[i]);
 	}
 }
