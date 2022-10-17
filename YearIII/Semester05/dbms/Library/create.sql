54cdebda60ae0fff150a35f59145ae1ac704d098
EugeneFelix <ontharros@gmail.com>
2022-10-17 23:18:50 +0530

    commit

--- YearIII/Semester05/dbms/Library/create.sql
+++ YearIII/Semester05/dbms/Library/create.sql
@@ -1,4 +1,4 @@
---Create Table PUBLISHER with Primary Key as NAME
+-- Create Table PUBLISHER with Primary Key as NAME.
 
 CREATE TABLE PUBLISHER
 (NAME VARCHAR(20) PRIMARY KEY,
@@ -7,7 +7,7 @@ ADDRESS VARCHAR(20));
 
 DESC PUBLISHER;
 
---Create Table BOOK with Primary Key as BOOK_ID and Foreign Key PUB_NAME referring the PUBLISHER table
+-- Create Table BOOK with Primary Key as BOOK_ID and Foreign Key PUB_NAME referring the PUBLISHER table.
 
 CREATE TABLE BOOK
 (BOOK_ID INTEGER PRIMARY KEY,
@@ -19,7 +19,7 @@ FOREIGN KEY (PUB_NAME) REFERENCES PUBLISHER(NAME) ON DELETE CASCADE);
 
 DESC BOOK;
 
---Create Table BOOK_AUTHORS with Primary Key as BOOK_ID and AUTHOR_NAME and Foreign Key BOOK_ID referring the BOOK table
+-- Create Table BOOK_AUTHORS with Primary Key as BOOK_ID and AUTHOR_NAME and Foreign Key BOOK_ID referring the BOOK table.
 
 CREATE TABLE BOOK_AUTHORS
 (AUTHOR_NAME VARCHAR(20),
@@ -29,7 +29,7 @@ PRIMARY KEY(BOOK_ID, AUTHOR_NAME));
 
 DESC BOOK_AUTHORS;
 
---Create Table LIBRARY_PROGRAMME with Primary Key as PROGRAMME_ID
+-- Create Table LIBRARY_PROGRAMME with Primary Key as PROGRAMME_ID.
 
 CREATE TABLE LIBRARY_PROGRAMME
 (PROGRAMME_ID INTEGER PRIMARY KEY,
@@ -38,7 +38,7 @@ ADDRESS VARCHAR(50));
 
 DESC LIBRARY_PROGRAMME;
 
---Create Table as BOOK_COPIES with Primary Key as BOOK_ID and PROGRAMME_ID and Foreign Key BOOK_ID and PROGRAMME_ID referring the BOOK and LIBRARY_PROGRAMME tables respectively
+-- Create Table as BOOK_COPIES with Primary Key as BOOK_ID and PROGRAMME_ID and Foreign Key BOOK_ID and PROGRAMME_ID referring the BOOK and LIBRARY_PROGRAMME tables respectively.
 
 CREATE TABLE BOOK_COPIES
 (NO_OF_COPIES INTEGER,
@@ -50,14 +50,14 @@ PRIMARY KEY (BOOK_ID,PROGRAMME_ID));
 
 DESC BOOK_COPIES;
 
--- Create Table CARD with Primary Key as CARD_NO
+-- Create Table CARD with Primary Key as CARD_NO.
 
 CREATE TABLE CARD
 (CARD_NO INTEGER PRIMARY KEY);
 
 DESC CARD;
 
--- Create Table BOOK_LENDING With Primary Key as BOOK_ID, PROGRAMME_ID and CARD_NO and Foreign key as BOOK_ID, PROGRAMME_ID and CARD_NO referring the BOOK, LIBRARY_PROGRAMME and CARD tables respectively
+-- Create Table BOOK_LENDING With Primary Key as BOOK_ID, PROGRAMME_ID and CARD_NO and Foreign key as BOOK_ID, PROGRAMME_ID and CARD_NO referring the BOOK, LIBRARY_PROGRAMME and CARD tables respectively.
 
 CREATE TABLE BOOK_LENDING
 (BOOK_ID INTEGER,
