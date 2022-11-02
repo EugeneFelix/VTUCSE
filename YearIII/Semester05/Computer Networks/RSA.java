ea767fb97f53cdd2467e96eaf68e3abe13a92e31
EugeneFelix <ontharros@gmail.com>
2022-11-02 21:17:05 +0530

    commit

--- YearIII/Semester05/Computer Networks/RSA.java
+++ YearIII/Semester05/Computer Networks/RSA.java
@@ -22,30 +22,23 @@ public class RSA {
 		System.out.println("Error message:");
 		message1 = br.readLine();
 		char[] msg = message1.toCharArray();
-		for (int i = 0; i < msg.length; i++) {
+		for (int i = 0; i < msg.length; i++)
 			plainText[i] = msg[i];
-		}
-		for (d = 0; d < z; d++) {
-			if ((e * d) % z == 1) {
+		for (d = 0; d < z; d++)
+			if ((e * d) % z == 1)
 				break;
-			}
-		}
 		System.out.println("p = " + p + "\tq = " + q + "\tn = " + n + "\tz = " + z + "\te = " + e + "\td = " + d);
 		System.out.println("Cipher Text = ");
-		for (int i = 0; i < msg.length; i++) {
+		for (int i = 0; i < msg.length; i++)
 			cipherText[i] = mult(plainText[i], e, n);
-		}
-		for (int i = 0; i < msg.length; i++) {
+		for (int i = 0; i < msg.length; i++)
 			System.out.print(cipherText[i] + "\t");
-		}
 		System.out.println();
 		System.out.println("Plain Text = ");
-		for (int i = 0; i < msg.length; i++) {
+		for (int i = 0; i < msg.length; i++)
 			plainText[i] = mult(cipherText[i], d, n);
-		}
-		for (int i = 0; i < msg.length; i++) {
+		for (int i = 0; i < msg.length; i++)
 			System.out.print((char) plainText[i]);
-		}
 		System.out.println();
 		sc.close();
 	}
