0843448ab9438d8687222a958a8aa2bcf929f642
EugeneFelix <ontharros@gmail.com>
2023-05-13 10:53:27 +0530

    commit

--- YearIII/Semester06/SS&CD/srparse2.c
+++ YearIII/Semester06/SS&CD/srparse2.c
@@ -1,63 +1,64 @@
 #include<stdio.h>
 #include<string.h>
 int k = 0, z = 0, i = 0, j = 0, c = 0;
-char a[16], ac[20], stk[17], act[10];
-void check();
+char input[25], stack[50];
+char reduce[] = "SHIFT->";
+char shift[] = "REDUCE TO E";
+
+void check() {
+  for (z = 0; z < c; z++)
+    if (stack[z] == 'i' && stack[z + 1] == 'd') {
+      stack[z] = 'E';
+      stack[z + 1] = '\0';
+      printf("\n$%s\t%s$\t%s", stack, input, reduce);
+      j++;
+    }
+  for (z = 0; z < c; z++)
+    if (stack[z] == 'E' && stack[z + 1] == '+' && stack[z + 2] == 'E') {
+      stack[z] = 'E';
+      stack[z + 1] = '\0';
+      stack[z + 2] = '\0';
+      printf("\n$%s\t%s$\t%s", stack, input, reduce);
+      i = i - 2;
+    } for (z = 0; z < c; z++)
+    if (stack[z] == 'E' && stack[z + 1] == '*' && stack[z + 2] == 'E') {
+      stack[z] = 'E';
+      stack[z + 1] = '\0';
+      stack[z + 1] = '\0';
+      printf("\n$%s\t%s$\t%s", stack, input, reduce);
+      i = i - 2;
+    } for (z = 0; z < c; z++)
+    if (stack[z] == '(' && stack[z + 1] == 'E' && stack[z + 2] == ')') {
+      stack[z] = 'E';
+      stack[z + 1] = '\0';
+      stack[z + 1] = '\0';
+      printf("\n$%s\t%s$\t%s", stack, input, reduce);
+      i = i - 2;
+    }
+}
+
 int main() {
   puts("GRAMMAR is E->E+E \n E->E*E \n E->(E) \n E->id");
-  puts("enter input string ");
-  scanf("%s", a);
-  c = strlen(a);
-  strcpy(act, "SHIFT->");
+  puts("Enter Input String");
+  scanf("%s", input);
+  c = strlen(input);
   puts("stack \t input \t action");
   for (k = 0, i = 0; j < c; k++, i++, j++) {
-    if (a[j] == 'i' && a[j + 1] == 'd') {
-      stk[i] = a[j];
-      stk[i + 1] = a[j + 1];
-      stk[i + 2] = '\0';
-      a[j] = ' ';
-      a[j + 1] = ' ';
-      printf("\n$%s\t%s$\t%sid", stk, a, act);
+    if (stack[j] == 'i' && stack[j + 1] == 'd') {
+      stack[i] = stack[j];
+      stack[i + 1] = stack[j + 1];
+      stack[i + 2] = '\0';
+      stack[j] = ' ';
+      stack[j + 1] = ' ';
+      printf("\n$%s\t%s$\t%sid", stack, input, shift);
       check();
     } else {
-      stk[i] = a[j];
-      stk[i + 1] = '\0';
-      a[j] = ' ';
-      printf("\n$%s\t%s$\t%ssymbols", stk, a, act);
+      stack[i] = stack[j];
+      stack[i + 1] = '\0';
+      stack[j] = ' ';
+      printf("\n$%s\t%s$\t%ssymbols", stack, input, shift);
       check();
     }
   }
   return 0;
-}
-void check() {
-  strcpy(ac, "REDUCE TO E");
-  for (z = 0; z < c; z++)
-    if (stk[z] == 'i' && stk[z + 1] == 'd') {
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
-      j++;
-    }
-  for (z = 0; z < c; z++)
-    if (stk[z] == 'E' && stk[z + 1] == '+' && stk[z + 2] == 'E') {
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-      stk[z + 2] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
-      i = i - 2;
-    } for (z = 0; z < c; z++)
-    if (stk[z] == 'E' && stk[z + 1] == '*' && stk[z + 2] == 'E') {
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-      stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
-      i = i - 2;
-    } for (z = 0; z < c; z++)
-    if (stk[z] == '(' && stk[z + 1] == 'E' && stk[z + 2] == ')') {
-      stk[z] = 'E';
-      stk[z + 1] = '\0';
-      stk[z + 1] = '\0';
-      printf("\n$%s\t%s$\t%s", stk, a, ac);
-      i = i - 2;
-    }
 }
