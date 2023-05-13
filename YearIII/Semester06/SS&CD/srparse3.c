3ac4a6e20c1b5f6c1959a87f588251e9897651d3
EugeneFelix <ontharros@gmail.com>
2023-05-13 11:12:23 +0530

    commit

--- YearIII/Semester06/SS&CD/srparse3.c
+++ YearIII/Semester06/SS&CD/srparse3.c
@@ -1,33 +1,33 @@
 #include<stdio.h>
 #include<string.h>
-int k = 0, z = 0, i = 0, j = 0;
+int i = 0, j = 0;
 char input[16], stack[50];
 char reduce[] = "REDUCE TO E";
 char shift[] = "SHIFT->";
 
 void check(int buflen) {
-  for (z = 0; z < buflen; z++)
+  for (int z = 0; z < buflen; z++)
     if (stack[z] == 'i' && stack[z + 1] == 'd') {
       stack[z] = 'E';
       stack[z + 1] = '\0';
       printf("\n$%s\t%s$\t%s", stack, input, reduce);
       j++;
     }
-  for (z = 0; z < buflen; z++)
+  for (int z = 0; z < buflen; z++)
     if (stack[z] == 'E' && stack[z + 1] == '+' && stack[z + 2] == 'E') {
       stack[z] = 'E';
       stack[z + 1] = '\0';
       stack[z + 2] = '\0';
       printf("\n$%s\t%s$\t%s", stack, input, reduce);
       i = i - 2;
-    } for (z = 0; z < buflen; z++)
+    } for (int z = 0; z < buflen; z++)
     if (stack[z] == 'E' && stack[z + 1] == '*' && stack[z + 2] == 'E') {
       stack[z] = 'E';
       stack[z + 1] = '\0';
       stack[z + 1] = '\0';
       printf("\n$%s\t%s$\t%s", stack, input, reduce);
       i = i - 2;
-    } for (z = 0; z < buflen; z++)
+    } for (int z = 0; z < buflen; z++)
     if (stack[z] == '(' && stack[z + 1] == 'E' && stack[z + 2] == ')') {
       stack[z] = 'E';
       stack[z + 1] = '\0';
@@ -43,7 +43,7 @@ int main() {
   scanf("%s", input);
   int buflen = strlen(input);
   puts("stack \t input \t action");
-  for (k = 0, i = 0; j < buflen; k++, i++, j++) {
+  for (int k = 0, i = 0; j < buflen; k++, i++, j++) {
     if (input[j] == 'i' && input[j + 1] == 'd') {
       stack[i] = input[j];
       stack[i + 1] = input[j + 1];
