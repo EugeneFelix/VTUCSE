dc66121262cf722579a533f478244d0b6d2419e1
EugeneFelix <ontharros@gmail.com>
2023-05-14 15:26:56 +0530

    commit

--- YearIII/Semester06/SS&CD/ll_parse_table.c
+++ YearIII/Semester06/SS&CD/ll_parse_table.c
@@ -3,18 +3,18 @@
 #include<stdio.h>
 
 char
-prod[3][10] = {
+prod[3][6] = {
     "A->aBa",
     "B->bB",
     "B->@"
 }, input[10], stack[25];
-int top = -1, i = 0, k, l;
+int top = -1, buflen = 0;
 
 void pop() {
     --top;
 }
 
-void push(char p) {
+void push_production(char p) {
     if (p == 'A') {
         pop();
         for (int i = strlen(prod[0]) - 1; i >= 3; i--)
@@ -39,7 +39,7 @@ void incorrect_input() {
 }
 
 void check_valid() {
-    if (stack[top] == '$' && input[i] == '$')
+    if (stack[top] == '$' && input[strlen(input)] == '$')
         printf("\n$\t$\nValid string Accepted\n");
     else
         printf("\nInvalid string rejected\n");
@@ -58,31 +58,34 @@ int main() {
     stack[++top] = '$';
     stack[++top] = 'A';
     printf("\n\nstack\tInput\taction\n\n----------\n");
-    while (i != strlen(input) && stack[top] != '$') {
+    while (buflen != strlen(input) && stack[top] != '$') {
         printf("\n");
-        for (l = top; l >= 0; l--)
-            printf("%c", stack[l]);
+        //column 1
+        for (int j = top; j >= 0; j--)
+            printf("%c", stack[j]);
         printf("\t");
-        for (l = i; l < strlen(input); l++)
-            printf("%c", input[l]);
+        //column 2
+        for (int j = buflen; j < strlen(input); j++)
+            printf("%c", input[j]);
         printf("\t");
+        //End column 2
         if (stack[top] == 'A') {
             printf("A->aBa");
-            push('A');
+            push_production('A');
         } else if (stack[top] == 'B') {
-            if (input[i] != 'b') {
-                printf("B->@\n\t Matched @");
+            if (input[buflen] != 'b') {
+                printf("B->@\tMatched @");
                 pop();
             } else {
                 printf("B->bB");
-                push('B');
+                push_production('B');
             }
         } else {
-            if (stack[top] == input[i]) {
-                printf("POP %c", input[i]);
-                printf("\tMatched %c", input[i]);
+            if (stack[top] == input[buflen]) {
+                printf("POP %c", input[buflen]);
+                printf("\tMatched %c", input[buflen]);
                 pop();
-                i++;
+                buflen++;
             } else
                 break;
         }
