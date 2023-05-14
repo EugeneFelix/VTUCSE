71a44a953718a248f40154e3b9f732687db489bf
EugeneFelix <ontharros@gmail.com>
2023-05-14 13:11:00 +0530

    commit

--- YearIII/Semester06/SS&CD/robin_vj.c
+++ YearIII/Semester06/SS&CD/robin_vj.c
@@ -1,53 +1,55 @@
 #include<stdio.h>
 
-int count, j, number_processes, time, flag = 0, time_quantum, choice = 0;
-int wait_time = 0, turnaround_time = 0, at[10], bt[10], rt[10];
-int endTime, i, smallest;
-int remain = 0, sum_wait = 0, sum_turnaround = 0;
+int current_process, number_processes, time_lapsed, flag = 0, time_quantum, choice = 0;
+int total_wait_time = 0, total_turnaround_time = 0, at[10], bt[10], rem_time[10];
+int endTime, smallest;
+int processes_remaining = 0, sum_wait = 0, sum_turnaround = 0;
 void round_robin() {
   printf("Enter Time Quantum:\t");
-  scanf("%d", & time_quantum);
-  remain = number_processes;
-  printf("\nProcess time|Turnaround Time|Waiting Time\n");
-  for (time = 0, count = 0; remain != 0;) {
-    if (rt[count] <= time_quantum && rt[count] > 0) {
-      time += rt[count];
-      rt[count] = 0;
+  scanf("%d", &time_quantum);
+  processes_remaining = number_processes;
+  printf("\nProcess Time|Turnaround Time|Waiting Time\n");
+  for (time_lapsed = 0, current_process = 0; processes_remaining != 0;) {
+    if (rem_time[current_process] <= time_quantum && rem_time[current_process] > 0) {
+      time_lapsed += rem_time[current_process];
+      rem_time[current_process] = 0;
       flag = 1;
-    } else if (rt[count] > 0) {
-      rt[count] -= time_quantum;
-      time += time_quantum;
+    } else if (rem_time[current_process] > 0) {
+      rem_time[current_process] -= time_quantum;
+      time_lapsed += time_quantum;
     }
-    if (rt[count] == 0 && flag == 1) {
-      remain--;
-      printf("P[%d]\t\t|\t%d\t\t|\t%d\n", count + 1, time - at[count], time - at[count] - bt[count]);
-      wait_time += time - at[count] - bt[count];
-      turnaround_time += time - at[count];
+    if (rem_time[current_process] == 0 && flag == 1) {
+      processes_remaining--;
+      printf("P[%d]\t\t|\t%d\t\t|\t%d\n", current_process + 1, time_lapsed - at[current_process], time_lapsed - at[current_process] - bt[current_process]);
+      /****************************/
+      total_wait_time += time_lapsed - at[current_process] - bt[current_process];
+      total_turnaround_time += time_lapsed - at[current_process];
+      /***************************/
       flag = 0;
     }
-    if (count == number_processes - 1)
-      count = 0;
-    else if (at[count + 1] <= time)
-      count++;
+    if (current_process == number_processes - 1)
+      current_process = 0;
+    else if (at[current_process + 1] <= time_lapsed)
+      current_process++;
     else
-      count = 0;
+      current_process = 0;
   }
-  printf("\nAverage Waiting Time= %.2f\n", wait_time * 1.0 / number_processes);
-  printf("Avg Turnaround Time = %.2f\n", turnaround_time * 1.0 / number_processes);
+  printf("\nAverage Waiting Time= %.2f\n", total_wait_time * 1.0 / number_processes);
+  printf("Avg Turnaround Time = %.2f\n", total_turnaround_time * 1.0 / number_processes);
 }
 void srt() {
-  remain = 0;
-  printf("\nProcesst|Turnaround Time| Waiting Timen\n");
-  rt[9] = 9999;
-  for (time = 0; remain != number_processes; time++) {
+  current_process = 0;
+  printf("\nProcess| Turnaround Time| Waiting Timen\n");
+  rem_time[9] = 9999;
+  for (time_lapsed = 0; current_process != number_processes; time_lapsed++) {
     smallest = 9;
-    for (i = 0; i < number_processes; i++)
-      if (at[i] <= time && rt[i] < rt[smallest] && rt[i] > 0)
+    for (int i = 0; i < number_processes; i++)
+      if (at[i] <= time_lapsed && rem_time[i] < rem_time[smallest] && rem_time[i] > 0)
         smallest = i;
-    rt[smallest]--;
-    if (rt[smallest] == 0) {
-      remain++;
-      endTime = time + 1;
+    rem_time[smallest]--;
+    if (rem_time[smallest] == 0) {
+      current_process++;
+      endTime = time_lapsed + 1;
       printf("\nP[%d]\t|\t%d\t|\t%d", smallest + 1, endTime - at[smallest], endTime - bt[smallest] - at[smallest]);
       printf("\n");
       sum_wait += endTime - bt[smallest] - at[smallest];
@@ -62,12 +64,12 @@ int main() {
   scanf("%d", &choice);
   printf("Enter no of Processes : ");
   scanf("%d", &number_processes);
-  for (i = 0; i < number_processes; i++) {
+  for (int i = 0; i < number_processes; i++) {
     printf("Enter arrival time for process P%d : ", i + 1);
     scanf("%d", &at[i]);
     printf("Enter burst time for process P%d :", i + 1);
     scanf("%d", &bt[i]);
-    rt[i] = bt[i];
+    rem_time[i] = bt[i];
   }
   switch (choice) {
   case 1:
