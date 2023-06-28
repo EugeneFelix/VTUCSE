f03331df2aaf02a2aee15bc0e5d655b287c6d19f
EugeneFelix <ontharros@gmail.com>
2023-06-28 22:52:51 +0530

    commit

--- YearIII/Semester06/SS&CD/ll_parse_table.c
+++ YearIII/Semester06/SS&CD/ll_parse_table.c
@@ -1,91 +1,93 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
-char
-prod[3][10] = {
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char productions[3][10] = {
 	"A->aBa",
 	"B->bB",
 	"B->@"
-}, input[10], stack[25];
-int top = -1, buflen = 0;
+};
+
+char inputString[10], stack[25];
+int top = -1, inputIndex = 0;
 
 void pop() {
 	--top;
 }
 
-void push_production(char p) {
-	if (p == 'A') {
-		pop();
-		for (int i = strlen(prod[0]) - 1; i >= 3; i--)
-		stack[++top] = prod[0][i];
-	} else if (p == 'B') {
-		pop();
-		for (int i = strlen(prod[1]) - 1; i >= 3; i--)
-		stack[++top] = prod[1][i];
-	}
+void pushProduction(char nonTerminal) {
+	int productionIndex = (nonTerminal == 'A') ? 0 : 1;
+	pop();
+	for (int i = strlen(productions[productionIndex]) - 1; i >= 3; i--)
+		stack[++top] = productions[productionIndex][i];
 }
-void incorrect_input() {
-	for (int i = 0; input[i] != '\0'; i++) {
-		if ((input[i] != 'a') && (input[i] != 'b') && (input[i] != '$')) {
+
+void validateInputString() {
+	for (int i = 0; inputString[i] != '\0'; i++) {
+		if (inputString[i] != 'a' && inputString[i] != 'b' && inputString[i] != '$') {
 			printf("Invalid string");
 			exit(0);
 		}
 	}
-	if (input[strlen(input) - 1] != '$') {
-		printf("\n\nInput string entered without end marker $");
+	if (inputString[strlen(inputString) - 1] != '$') {
+		printf("\n\nInput string entered without end marker $\n");
 		exit(0);
 	}
 }
 
-void check_valid() {
-	if (stack[top] == '$' && input[buflen] == '$')
+void checkValidity() {
+	if (stack[top] == '$' && inputString[inputIndex] == '$')
 		printf("\n$\t$\nValid string Accepted\n");
 	else
 		printf("\nInvalid string rejected\n");
 }
+
 int main() {
-	printf("first(A)={a}\t");
-	printf("follow(A)={$}\n");
-	printf("first(B)={b,@}\t");
-	printf("follow(B)={a}\n\n");
+	printf("First(A) = {a}\t");
+	printf("Follow(A) = {$}\n");
+	printf("First(B) = {b,@}\t");
+	printf("Follow(B) = {a}\n\n");
 	printf("\t a \t b \t $ \n");
-	printf("A\t%s\n", prod[0]);
-	printf("B\t%s\t%s\n", prod[2], prod[1]);
-	printf("Enter the input string terminated with $ to parse:-");
-	strcpy(input, "abba");
-	incorrect_input();
+	printf("A\t%s\n", productions[0]);
+	printf("B\t%s\t%s\n", productions[2], productions[1]);
+	strcpy(inputString, "abba$");
+	validateInputString();
+
 	stack[++top] = '$';
 	stack[++top] = 'A';
+
 	printf("\n\nstack\tInput\taction\n\n----------\n");
-	while (buflen != strlen(input) && stack[top] != '$') {
+
+	while (inputIndex != strlen(inputString) && stack[top] != '$') {
 		printf("\n");
 		for (int j = top; j >= 0; j--)
-		printf("%c", stack[j]);
+			printf("%c", stack[j]);
 		printf("\t");
-		for (int j = buflen; j < strlen(input); j++)
-		printf("%c", input[j]);
+		for (int j = inputIndex; j < strlen(inputString); j++)
+			printf("%c", inputString[j]);
 		printf("\t");
+
 		if (stack[top] == 'A') {
 			printf("A->aBa");
-			push_production('A');
+			pushProduction('A');
 		} else if (stack[top] == 'B') {
-			if (input[buflen] == 'b') {
+			if (inputString[inputIndex] == 'b') {
 				printf("B->bB");
-				push_production('B');
+				pushProduction('B');
 			} else {
 				printf("B->@\tMatched @");
 				pop();
 			}
 		} else {
-			if (stack[top] == input[buflen]) {
-				printf("POP %c", input[buflen]);
-				printf("\tMatched %c", input[buflen]);
+			if (stack[top] == inputString[inputIndex]) {
+				printf("POP %c\tMatched %c", inputString[inputIndex], inputString[inputIndex]);
 				pop();
-				buflen++;
+				inputIndex++;
 			} else
-			break;
+				break;
 		}
 	}
-	check_valid();
+
+	checkValidity();
 	return 0;
 }
