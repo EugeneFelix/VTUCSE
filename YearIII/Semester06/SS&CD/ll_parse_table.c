3b7b417a0a77db9d90126d0f148cbaabc4eb5e83
EugeneFelix <ontharros@gmail.com>
2023-07-14 11:57:59 +0530

    commit

--- YearIII/Semester06/SS&CD/ll_parse_table.c
+++ YearIII/Semester06/SS&CD/ll_parse_table.c
@@ -1,88 +1,75 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
 
-char productions[3][10] = {
+using namespace std;
+
+vector<string> productions = {
 	"A->aBa",
 	"B->bB",
 	"B->@"
 };
 
-char inputString[10], stack[25];
-int top = -1, inputIndex = 0;
-
-void pop() {
-	--top;
-}
+string inputString;
+vector<char> stack;
+size_t inputIndex = 0;
 
 void pushProduction(char nonTerminal) {
 	int productionIndex = (nonTerminal == 'A') ? 0 : 1;
-	pop(); // top--
-	for (int i = strlen(productions[productionIndex]) - 1; i >= 3; i--)
-		stack[++top] = productions[productionIndex][i];
+	stack.pop_back();
+	for (int i = productions[productionIndex].size() - 1; i >= 3; --i)
+		stack.push_back(productions[productionIndex][i]);
 }
 
 void validateInputString() {
-	for (int i = 0; inputString[i] != '\0'; i++) {
-		if (inputString[i] != 'a' && inputString[i] != 'b' && inputString[i] != '$') {
-			printf("Invalid string");
+	for (char c : inputString) {
+		if (c != 'a' && c != 'b' && c != '$') {
+			cout << "Invalid string" << endl;
 			exit(0);
 		}
 	}
-	if (inputString[strlen(inputString) - 1] != '$') {
-		printf("\n\nInput string entered without end marker $\n");
+	if (inputString.back() != '$') {
+		cout << "\n\nInput string entered without end marker $\n";
 		exit(0);
 	}
 }
 
 void checkValidity() {
-	if (stack[top] == '$' && inputString[inputIndex] == '$')
-		printf("\n$\t$\nValid string Accepted\n");
+	if (stack.back() == '$' && inputString[inputIndex] == '$')
+		cout << "\n$\t$\nValid string Accepted\n";
 	else
-		printf("\nInvalid string rejected\n");
+		cout << "\nInvalid string rejected\n";
 }
 
 int main() {
-	printf("First(A) = {a}\t");
-	printf("Follow(A) = {$}\n");
-	printf("First(B) = {b,@}\t");
-	printf("Follow(B) = {a}\n\n");
-	printf("\t a \t b \t $ \n");
-	printf("A\t%s\n", productions[0]);
-	printf("B\t%s\t%s\n", productions[2], productions[1]);
-	strcpy(inputString, "abba$");
+	inputString = "abba$";
 	validateInputString();
+	stack = {'$', 'A'};
+	cout << "\n\nstack\tInput\taction\n\n----------------------\n";
+	while (inputIndex != inputString.size() && stack.back() != '$') {
+		cout << endl;
+		for (auto it = stack.rbegin(); it != stack.rend(); ++it)
+			cout << *it;
+		cout << "\t";
+		cout << inputString.substr(inputIndex);
+		cout << "\t";
 
-	stack[++top] = '$';
-	stack[++top] = 'A';
-
-	printf("\n\nstack\tInput\taction\n\n----------\n");
-
-	while (inputIndex != strlen(inputString) && stack[top] != '$') {
-		printf("\n");
-		for (int j = top; j >= 0; j--)
-			printf("%c", stack[j]);
-		printf("\t");
-		for (int j = inputIndex; j < strlen(inputString); j++)
-			printf("%c", inputString[j]);
-		printf("\t");
-
-		if (stack[top] == 'A') {
-			printf("A->aBa");
+		if (stack.back() == 'A') {
+			cout << "A->aBa";
 			pushProduction('A');
-		} else if (stack[top] == 'B') {
+		} else if (stack.back() == 'B') {
 			if (inputString[inputIndex] == 'b') {
-				printf("B->bB");
+				cout << "B->bB";
 				pushProduction('B');
 			} else {
-				printf("B->@\tMatched @");
-				pop();
+				cout << "B->@\tMatched @";
+				stack.pop_back();
 			}
 		} else {
-			if (stack[top] == inputString[inputIndex]) {
-				printf("POP %c\tMatched %c", inputString[inputIndex], inputString[inputIndex]);
-				pop();
-				inputIndex++;
+			if (stack.back() == inputString[inputIndex]) {
+				cout << "POP " << inputString[inputIndex] << "\tMatched " << inputString[inputIndex];
+				stack.pop_back();
+				++inputIndex;
 			} else
 				break;
 		}
