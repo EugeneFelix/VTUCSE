b80a315874c42862fd1b37329ccdf688b20872ac
EugeneFelix <ontharros@gmail.com>
2023-07-14 10:55:43 +0530

    commit

--- YearIII/Semester06/SS&CD/triples_asm.cpp
+++ YearIII/Semester06/SS&CD/triples_asm.cpp
@@ -7,33 +7,33 @@ using namespace std;
 int main() {
 	ifstream inputFile("./inputs/asmin.c");
 	ofstream outputFile("./inputs/asmout.a");
-
 	string op, arg1, arg2, result;
+	
 	while (inputFile >> result >> arg1 >> op >> arg2) {
 		switch (op[0]) {
 			case '+':
-				outputFile << "\nMOV R0," << arg1;
-				outputFile << "\nADD R0," << arg2;
-				outputFile << "\nMOV " << result << ",R0";
+				outputFile << "\nMOV R0, " << arg1;
+				outputFile << "\nADD R0, " << arg2;
+				outputFile << "\nMOV " << result << ", R0";
 				break;
 			case '*':
-				outputFile << "\nMOV R0," << arg1;
-				outputFile << "\nMUL R0," << arg2;
-				outputFile << "\nMOV " << result << ",R0";
+				outputFile << "\nMOV R0, " << arg1;
+				outputFile << "\nMUL R0, " << arg2;
+				outputFile << "\nMOV " << result << ", R0";
 				break;
 			case '-':
-				outputFile << "\nMOV R0," << arg1;
-				outputFile << "\nSUB R0," << arg2;
-				outputFile << "\nMOV " << result << ",R0";
+				outputFile << "\nMOV R0, " << arg1;
+				outputFile << "\nSUB R0, " << arg2;
+				outputFile << "\nMOV " << result << ", R0";
 				break;
 			case '/':
-				outputFile << "\nMOV R0," << arg1;
-				outputFile << "\nDIV R0," << arg2;
-				outputFile << "\nMOV " << result << ",R0";
+				outputFile << "\nMOV R0, " << arg1;
+				outputFile << "\nDIV R0, " << arg2;
+				outputFile << "\nMOV " << result << ", R0";
 				break;
 			case '=':
-				outputFile << "\nMOV R0," << arg1;
-				outputFile << "\nMOV " << result << ",R0";
+				outputFile << "\nMOV R0, " << arg1;
+				outputFile << "\nMOV " << result << ", R0";
 				break;
 			default:
 				break;
