8a5d53f8cfd593e796a6a36ff6f51d652fc39159
nonomino <ontharros@gmail.com>
2023-11-17 19:47:47 +0530

    commit

--- YearIV/AIML/candidate_elim.py
+++ YearIV/AIML/candidate_elim.py
@@ -1,35 +1,51 @@
 import csv
 
-with open("trainingexamples.csv") as f:
-    csv_file = csv.reader(f)
-    data = list(csv_file)
+path = '.././data/candidate_elim.csv'
+data = []
 
-    specific = data[0][:-1]
-    general = [['?' for i in range(len(specific))] for j in range(len(specific))]
+with open(path, 'r') as file:
+	reader = csv.reader(file)
+	for row in reader:
+		data.append(row)
 
-    for i in data:
-        if i[-1] == "Yes":
-            for j in range(len(specific)):
-                if i[j] != specific[j]:
-                    specific[j] = "?"
-                    general[j][j] = "?"
+print(data)
+concepts = [row[:-1] for row in data]
+target = [row[-1] for row in data]
 
-        elif i[-1] == "No":
-            for j in range(len(specific)):
-                if i[j] != specific[j]:
-                    general[j][j] = specific[j]
-                else:
-                    general[j][j] = "?"
+def learn(concepts, target):
+	specific_h = concepts[0].copy()
+	print("\nInitialization of specific_h and general_h")
+	print("\nSpecific Boundary: ", specific_h)
+	general_h = [["?" for i in range(len(specific_h))] for i in range(len(specific_h))]
+	print("\nGeneric Boundary: ", general_h)
 
-        print("\nStep " + str(data.index(i)+1) + " of Candidate Elimination Algorithm")
-        print(specific)
-        print(general)
+	for i, h in enumerate(concepts):
+		print("\nInstance", i + 1, "is", h)
+		if target[i] == "yes":
+			print("Instance is Positive")
+			for x in range(len(specific_h)):
+				if h[x] != specific_h[x]:
+					specific_h[x] = '?'
+					general_h[x][x] = '?'
 
-    gh = [] # gh = general Hypothesis
-    for i in general:
-        for j in i:
-            if j != '?':
-                gh.append(i)
-                break
-    print("\nFinal Specific hypothesis:\n", specific)
-    print("\nFinal General hypothesis:\n", gh)
+		if target[i] == "no":
+			print("Instance is Negative")
+			for x in range(len(specific_h)):
+				if h[x] != specific_h[x]:
+					general_h[x][x] = specific_h[x]
+				else:
+					general_h[x][x] = '?'
+
+		print("Specific Boundary after", i + 1, "Instance is", specific_h)
+		print("Generic Boundary after", i + 1, "Instance is", general_h)
+		print("\n")
+
+	indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
+	for i in indices:
+		general_h.remove(['?', '?', '?', '?', '?', '?'])
+	return specific_h, general_h
+
+s_final, g_final = learn(concepts, target)
+
+print("Final Specific_h: ", s_final, sep="\n")
+print("Final General_h: ", g_final, sep="\n")
--- YearIV/AIML/id3.py
+++ YearIV/AIML/id3.py
@@ -1,42 +1,78 @@
-import pandas as pd
-from pprint import pprint
-from sklearn.feature_selection import mutual_info_classif
-from collections import Counter
+import csv
+import math
 
-def id3(df, target_attribute, attribute_names, default_class = None):
-	cnt = Counter(x for x in df[target_attribute])
-	if len(cnt) == 1:
-		return next(iter(cnt))
-	
-	elif df.empty or (not attribute_names):
-		 return default_class
+class Node:
+	def __init__(self):
+		self.child_nodes = []
+		self.attribute_value = ""
+		self.is_leaf = False
+		self.predicted_label = ""
 
-	else:
-		gainz = mutual_info_classif(df[attribute_names],df[target_attribute],discrete_features = True)
-		index_of_max = gainz.tolist().index(max(gainz))
-		best_attr = attribute_names[index_of_max]
-		tree = {best_attr:{}}
-		remaining_attribute_names = [i for i in attribute_names if i! = best_attr]
-		
-		for attr_val, data_subset in df.groupby(best_attr):
-			subtree = id3(data_subset, target_attribute, remaining_attribute_names,default_class)
-			tree[best_attr][attr_val] = subtree
-		
-		return tree
-	
+def read_csv(file_path):
+	with open(file_path, 'r') as file:
+		reader = csv.DictReader(file)
+		return [row for row in reader]
 
-df = pd.read_csv("p-tennis.csv")
+def calculate_entropy(examples):
+	counts = {"yes": 0, "no": 0}
+	for row in examples:
+		counts[row["answer"]] += 1
+	probs = [count / len(examples) for count in counts.values()]
+	return -sum(p * math.log2(p) if p != 0 else 0 for p in probs)
 
-attribute_names = df.columns.tolist()
-print("List of attribut name")
+def calculate_info_gain(examples, attr):
+	unique_values = set(row[attr] for row in examples)
+	entropy = calculate_entropy(examples)
+	for value in unique_values:
+		subdata = [row for row in examples if row[attr] == value]
+		entropy -= (len(subdata) / len(examples)) * calculate_entropy(subdata)
+	return entropy
 
-attribute_names.remove("PlayTennis")
+def build_id3_tree(examples, attributes):
+	root = Node()
+	max_feature = max(attributes, key=lambda feature: calculate_info_gain(examples, feature))
+	root.attribute_value = max_feature
+	unique_values = set(row[max_feature] for row in examples)
+	for value in unique_values:
+		subdata = [row for row in examples if row[max_feature] == value]
+		if calculate_entropy(subdata) == 0:
+			new_node = Node()
+			new_node.is_leaf = True
+			new_node.attribute_value = value
+			new_node.predicted_label = {row["answer"] for row in subdata}
+			root.child_nodes.append(new_node)
+		else:
+			dummy_node = Node()
+			dummy_node.attribute_value = value
+			new_attributes = attributes.copy()
+			new_attributes.remove(max_feature)
+			child = build_id3_tree(subdata, new_attributes)
+			dummy_node.child_nodes.append(child)
+			root.child_nodes.append(dummy_node)
+	return root
 
-for colname in df.select_dtypes("object"):
-	df[colname], _ = df[colname].factorize()
-	
-print(df)
+def print_tree(root, depth=0):
+	print("\t" * depth + root.attribute_value, "->", root.predicted_label if root.is_leaf else "")
+	for child in root.child_nodes:
+		print_tree(child, depth + 1)
 
-tree =  id3(df,"PlayTennis", attribute_names)
-print("The tree structure")
-pprint(tree)
+def classify_example(root, new_example):
+	for child in root.child_nodes:
+		if child.attribute_value == new_example[root.attribute_value]:
+			if child.is_leaf:
+				print("Predicted Label for new example", new_example, " is:", child.predicted_label)
+				return
+			else:
+				classify_example(child.child_nodes[0], new_example)
+
+data = read_csv("./data/id3.csv")
+input_features = list(data[0].keys())
+input_features.remove("answer")
+
+id3_tree = build_id3_tree(data, input_features)
+print("Decision Tree is:")
+print_tree(id3_tree)
+print("------------------")
+
+new_sample = {"outlook": "sunny", "temperature": "hot", "humidity": "normal", "wind": "strong"}
+classify_example(id3_tree, new_sample)
