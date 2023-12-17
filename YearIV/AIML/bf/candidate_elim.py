a973bd7987e6a96105c5cb7a5ead895f3148587f
nonomino <ontharros@gmail.com>
2023-12-17 10:20:38 +0530

    commit

--- YearIV/AIML/bf/candidate_elim.py
+++ YearIV/AIML/bf/candidate_elim.py
@@ -1,11 +1,16 @@
 import numpy as np 
 import pandas as pd
 
-data = pd.read_csv(path+'../data/candidate_elimination.csv')
+data = pd.read_csv('../data/candidate_elimination.csv')
 concepts = np.array(data.iloc[:,0:-1])
 print("\nInstances are:\n",concepts)
 target = np.array(data.iloc[:,-1])
 print("\nTarget Values are: ",target)
+
+s_final, g_final = learn(concepts, target)
+
+print("Final Specific_h: ", s_final, sep="\n")
+print("Final General_h: ", g_final, sep="\n")
 
 def learn(concepts, target): 
 	specific_h = concepts[0].copy()
@@ -26,7 +31,7 @@ def learn(concepts, target):
 		if target[i] == "no":
 			print("Instance is Negative ")
 			for x in range(len(specific_h)): 
-				if h[x]!= specific_h[x]:
+				if h[x] != specific_h[x]:
 					general_h[x][x] = specific_h[x]	
 				else:
 					general_h[x][x] = '?'
@@ -39,8 +44,3 @@ def learn(concepts, target):
 	for i in indices:   
 		general_h.remove(['?', '?', '?', '?', '?', '?']) 
 	return specific_h, general_h 
-
-s_final, g_final = learn(concepts, target)
-
-print("Final Specific_h: ", s_final, sep="\n")
-print("Final General_h: ", g_final, sep="\n")
